b1efcf8a705aa247e0ef0c76eab07ff5e32fa755
posch-m <posch-m@web.de>
2026-07-27 14:55:09 +0000

    FraktionsFinder 1848 Version 1.0

--- fraktionen.js
+++ fraktionen.js
@@ -2,186 +2,249 @@
 ========================================================
 FraktionsFinder 1848
 fraktionen.js
+Teil 1 von 2
 ========================================================
 */
 
 const factions = {
 
-    casino: {
+casino:{
 
-        name: "Casino",
+    name:"Casino",
 
-        color: "#1f4e79",
+    color:"#1f4e79",
 
-        shortDescription:
-            "Du vertrittst gemäßigt liberale Positionen und befürwortest eine konstitutionelle Monarchie.",
+    ideology:"Nationalliberal",
 
-        description:
-            "Die Fraktion Casino war die größte liberale Gruppierung der Frankfurter Nationalversammlung. Sie setzte sich für einen deutschen Nationalstaat mit einer Verfassung, Grundrechten und einem erblichen Kaiser ein. Revolutionäre Veränderungen lehnte sie überwiegend ab.",
+    shortDescription:
+        "Du vertrittst gemäßigt liberale Positionen. Du befürwortest einen deutschen Nationalstaat mit einer konstitutionellen Monarchie.",
 
-        positions: [
-            "Konstitutionelle Monarchie",
-            "Erblicher Kaiser",
-            "Liberale Grundrechte",
-            "Rechtsstaat",
-            "Nationalstaat",
-            "Schrittweise Reformen"
-        ]
+    description:
+        "Das Casino war die größte und einflussreichste Fraktion der Frankfurter Nationalversammlung. Ihre Mitglieder wollten Deutschland unter einem erblichen Kaiser einigen, gleichzeitig aber eine moderne Verfassung, einen Rechtsstaat und garantierte Grundrechte schaffen. Revolutionäre Veränderungen lehnten sie überwiegend ab und setzten stattdessen auf Reformen.",
 
-    },
+    representatives:[
+        "Heinrich von Gagern",
+        "Eduard Simson",
+        "Georg Beseler"
+    ],
 
-    wuerttembergerhof: {
+    positions:[
+        "Konstitutionelle Monarchie",
+        "Erblicher Kaiser",
+        "Kleindeutsche Lösung",
+        "Rechtsstaat",
+        "Grundrechte",
+        "Nationalstaat",
+        "Schrittweise Reformen"
+    ]
 
-        name: "Württemberger Hof",
+},
 
-        color: "#4f81bd",
+wuerttembergerhof:{
 
-        shortDescription:
-            "Du stehst für einen gemäßigten Liberalismus mit föderalen Strukturen.",
+    name:"Württemberger Hof",
 
-        description:
-            "Der Württemberger Hof vertrat liberale, aber eher föderale Positionen. Die Rechte der Einzelstaaten sollten erhalten bleiben. Gleichzeitig unterstützte die Fraktion eine Verfassung und die Sicherung der Grundrechte.",
+    color:"#4f81bd",
 
-        positions: [
-            "Föderalismus",
-            "Konstitutionelle Monarchie",
-            "Grundrechte",
-            "Parlamentarische Mitwirkung",
-            "Ausgleich zwischen Fürsten und Parlament"
-        ]
+    ideology:"Liberalkonservativ",
 
-    },
+    shortDescription:
+        "Du vertrittst liberale Reformen, möchtest aber den deutschen Einzelstaaten viele Rechte erhalten.",
 
-    landsberg: {
+    description:
+        "Der Württemberger Hof stand dem Casino nahe, legte jedoch größeren Wert auf den Föderalismus. Die Fraktion unterstützte eine konstitutionelle Monarchie und eine Verfassung, wollte aber die Selbstständigkeit der deutschen Einzelstaaten stärker bewahren.",
 
-        name: "Landsberg",
+    representatives:[
+        "Karl Mathy",
+        "Johann Gustav Heckscher"
+    ],
 
-        color: "#5b9bd5",
+    positions:[
+        "Konstitutionelle Monarchie",
+        "Föderalismus",
+        "Grundrechte",
+        "Verfassung",
+        "Zusammenarbeit mit den Fürsten",
+        "Parlamentarische Mitwirkung"
+    ]
 
-        shortDescription:
-            "Du bevorzugst einen liberalen Verfassungsstaat mit stärkerer parlamentarischer Kontrolle.",
+},
 
-        description:
-            "Die Fraktion Landsberg stand zwischen gemäßigten Liberalen und Demokraten. Sie wollte den Einfluss des Parlaments ausbauen und die politische Mitbestimmung erweitern.",
+landsberg:{
 
-        positions: [
-            "Liberale Verfassung",
-            "Starkes Parlament",
-            "Mehr Mitbestimmung",
-            "Grundrechte",
-            "Verantwortliche Regierung"
-        ]
+    name:"Landsberg",
 
-    },
+    color:"#5b9bd5",
 
-    augsburgerhof: {
+    ideology:"Linksliberal",
 
-        name: "Augsburger Hof",
+    shortDescription:
+        "Du bevorzugst einen liberalen Verfassungsstaat mit einem starken Parlament und einer verantwortlichen Regierung.",
 
-        color: "#70ad47",
+    description:
+        "Die Fraktion Landsberg bildete den Übergang zwischen den gemäßigten Liberalen und den Demokraten. Sie wollte den Einfluss des Parlaments deutlich stärken, hielt aber grundsätzlich an einer konstitutionellen Monarchie fest.",
 
-        shortDescription:
-            "Du vertrittst demokratisch-liberale Positionen mit einem starken Parlament.",
+    representatives:[
+        "Friedrich Daniel Bassermann",
+        "Carl Theodor Welcker"
+    ],
 
-        description:
-            "Der Augsburger Hof gehörte zum linken Zentrum. Er setzte sich für eine weitreichende Parlamentarisierung, demokratische Reformen und eine stärkere Beteiligung der Bevölkerung ein.",
+    positions:[
+        "Starkes Parlament",
+        "Verantwortliche Regierung",
+        "Grundrechte",
+        "Konstitutionelle Monarchie",
+        "Liberale Reformpolitik",
+        "Nationale Einheit"
+    ]
 
-        positions: [
-            "Demokratische Reformen",
-            "Parlamentarische Regierung",
-            "Breiteres Wahlrecht",
-            "Grundrechte",
-            "Stärkere Bürgerbeteiligung"
-        ]
+},
 
-    },
+cafemilani:{
 
-    westendhall: {
+    name:"Café Milani",
 
-        name: "Westendhall",
+    color:"#8064a2",
 
-        color: "#ffc000",
+    ideology:"Liberales Zentrum",
 
-        shortDescription:
-            "Du unterstützt demokratische Reformen und eine deutliche Begrenzung monarchischer Macht.",
+    shortDescription:
+        "Du suchst den Ausgleich zwischen gemäßigten Liberalen und Demokraten und bevorzugst politische Kompromisse.",
 
-        description:
-            "Die Westendhall gehörte zum linken Flügel der Nationalversammlung. Sie trat für demokratische Mitbestimmung, ein möglichst allgemeines Wahlrecht und eine starke Rolle des Parlaments ein.",
+    description:
+        "Das Café Milani war eine kleinere Gruppierung im liberalen Zentrum. Die Abgeordneten unterstützten eine freiheitliche Verfassung, parlamentarische Reformen und eine stärkere politische Beteiligung der Bevölkerung, versuchten aber häufig zwischen den politischen Lagern zu vermitteln.",
 
-        positions: [
-            "Demokratie",
-            "Allgemeines Wahlrecht",
-            "Parlamentarismus",
-            "Bürgerrechte",
-            "Begrenzung der Monarchie"
-        ]
+    representatives:[
+        "Johannes von Miquel"
+    ],
 
-    },
+    positions:[
+        "Kompromisspolitik",
+        "Grundrechte",
+        "Parlamentarismus",
+        "Liberale Reformen",
+        "Mehr Bürgerbeteiligung",
+        "Nationalstaat"
+    ]
 
-    deutscherhof: {
+},augsburgerhof:{
 
-        name: "Deutscher Hof",
+    name:"Augsburger Hof",
 
-        color: "#ed7d31",
+    color:"#70ad47",
 
-        shortDescription:
-            "Du vertrittst konsequent demokratische Positionen und möchtest die politische Beteiligung deutlich ausweiten.",
+    ideology:"Demokratisch-liberal",
 
-        description:
-            "Der Deutsche Hof war eine linke demokratische Fraktion. Sie forderte weitgehende politische Mitbestimmung, soziale Reformen und eine stärkere Kontrolle der Regierung durch das Parlament.",
+    shortDescription:
+        "Du setzt dich für einen demokratischen Verfassungsstaat mit einem starken Parlament und einer breiten politischen Beteiligung ein.",
 
-        positions: [
-            "Demokratie",
-            "Allgemeines Wahlrecht",
-            "Soziale Reformen",
-            "Parlamentarische Kontrolle",
-            "Bürgerrechte"
-        ]
+    description:
+        "Der Augsburger Hof gehörte zum linken Zentrum der Frankfurter Nationalversammlung. Die Fraktion unterstützte demokratische Reformen, ein erweitertes Wahlrecht und eine stärkere parlamentarische Kontrolle der Regierung. Sie strebte Veränderungen auf gesetzlichem Weg an und grenzte sich von radikalen Revolutionären ab.",
 
-    },
+    representatives:[
+        "Robert Blum",
+        "Franz Jacob Wigard"
+    ],
 
-    donnersberg: {
+    positions:[
+        "Demokratischer Verfassungsstaat",
+        "Erweitertes Wahlrecht",
+        "Parlamentarische Regierung",
+        "Grundrechte",
+        "Nationale Einheit",
+        "Friedliche Reformen"
+    ]
 
-        name: "Donnersberg",
+},
 
-        color: "#c00000",
+westendhall:{
 
-        shortDescription:
-            "Du stehst für eine radikaldemokratische und republikanische Politik.",
+    name:"Westendhall",
 
-        description:
-            "Der Donnersberg war die am weitesten links stehende Fraktion der Frankfurter Nationalversammlung. Sie befürwortete eine Republik, allgemeines Wahlrecht, soziale Reformen und akzeptierte notfalls auch revolutionäre Veränderungen.",
+    color:"#ffc000",
 
-        positions: [
-            "Republik",
-            "Volkssouveränität",
-            "Allgemeines Wahlrecht",
-            "Soziale Gerechtigkeit",
-            "Revolution als letztes Mittel"
-        ]
+    ideology:"Linke Demokraten",
 
-    },
+    shortDescription:
+        "Du vertrittst demokratische Positionen und möchtest die Macht von Monarchen deutlich begrenzen.",
 
-    cafemilani: {
+    description:
+        "Die Fraktion Westendhall gehörte zum linken Flügel der Nationalversammlung. Sie forderte eine weitgehende Demokratisierung des politischen Systems, ein allgemeines Wahlrecht und eine Regierung, die dem Parlament verantwortlich ist. Im Gegensatz zum Donnersberg setzte sie jedoch stärker auf parlamentarische Mehrheiten als auf revolutionäre Mittel.",
 
-        name: "Café Milani",
+    representatives:[
+        "Johann Jacoby"
+    ],
 
-        color: "#8064a2",
+    positions:[
+        "Demokratie",
+        "Allgemeines Wahlrecht",
+        "Starkes Parlament",
+        "Bürgerrechte",
+        "Begrenzung der Monarchie",
+        "Politische Reformen"
+    ]
 
-        shortDescription:
-            "Du nimmst eine vermittelnde Position zwischen Liberalismus und Demokratie ein.",
+},
 
-        description:
-            "Das Café Milani bildete eine kleinere Gruppierung zwischen liberalen und demokratischen Fraktionen. Es unterstützte Grundrechte, parlamentarische Reformen und eine stärkere politische Beteiligung, suchte dabei aber häufig den Kompromiss.",
+deutscherhof:{
 
-        positions: [
-            "Kompromisspolitik",
-            "Grundrechte",
-            "Parlamentarismus",
-            "Mehr Bürgerbeteiligung",
-            "Liberale Reformen"
-        ]
+    name:"Deutscher Hof",
 
-    }
+    color:"#ed7d31",
+
+    ideology:"Radikaldemokratisch",
+
+    shortDescription:
+        "Du befürwortest weitreichende demokratische Reformen und eine umfassende politische Mitbestimmung der Bevölkerung.",
+
+    description:
+        "Der Deutsche Hof gehörte zum linken Lager der Paulskirche. Seine Mitglieder verlangten ein allgemeines Wahlrecht, eine parlamentarisch verantwortliche Regierung und umfangreiche politische Freiheitsrechte. Auch soziale Reformen spielten eine größere Rolle als bei den liberalen Fraktionen.",
+
+    representatives:[
+        "Ludwig Simon",
+        "Moritz Hartmann"
+    ],
+
+    positions:[
+        "Volkssouveränität",
+        "Allgemeines Wahlrecht",
+        "Parlamentarische Demokratie",
+        "Soziale Reformen",
+        "Bürgerrechte",
+        "Nationale Einheit"
+    ]
+
+},
+
+donnersberg:{
+
+    name:"Donnersberg",
+
+    color:"#c00000",
+
+    ideology:"Republikanisch-radikaldemokratisch",
+
+    shortDescription:
+        "Du stehst für eine Republik, umfassende Demokratie und tiefgreifende politische sowie soziale Veränderungen.",
+
+    description:
+        "Der Donnersberg war die linksradikalste Fraktion der Frankfurter Nationalversammlung. Ihre Mitglieder lehnten die Monarchie grundsätzlich ab und forderten eine demokratische Republik, allgemeines Wahlrecht sowie weitreichende Freiheits- und Mitbestimmungsrechte. Viele Abgeordnete hielten revolutionären Widerstand für legitim, wenn die Ziele der Revolution von 1848 nicht auf parlamentarischem Weg erreicht werden konnten.",
+
+    representatives:[
+        "Arnold Ruge",
+        "Friedrich Hecker",
+        "Gustav Struve"
+    ],
+
+    positions:[
+        "Republik",
+        "Volkssouveränität",
+        "Allgemeines Wahlrecht",
+        "Soziale Gerechtigkeit",
+        "Umfassende Grundrechte",
+        "Revolution als letztes Mittel"
+    ]
+
+}
 
 };
--- questions.js
+++ questions.js
@@ -2,148 +2,147 @@
 ========================================================
 FraktionsFinder 1848
 questions.js
+Teil 1 von 2
 ========================================================
 */
 
 const questions = [
 
 {
-    text: "Deutschland sollte einen erblichen Kaiser als Staatsoberhaupt erhalten.",
+    text: "Deutschland soll eine konstitutionelle Monarchie mit einem erblichen Kaiser als Staatsoberhaupt erhalten.",
     weights: {
-        casino: 2,
-        wuerttembergerhof: 2,
-        landsberg: 1,
-        augsburgerhof: 0,
-        westendhall: -1,
-        deutscherhof: -2,
-        donnersberg: -3,
-        cafemilani: -1
+        casino: 3,
+        wuerttembergerhof: 3,
+        landsberg: 2,
+        cafemilani: 1,
+        augsburgerhof: -1,
+        westendhall: -2,
+        deutscherhof: -3,
+        donnersberg: -3
     }
 },
 
 {
-    text: "Alle erwachsenen Männer sollten das gleiche Wahlrecht besitzen.",
+    text: "Deutschland sollte statt einer Monarchie eine Republik werden.",
     weights: {
-        casino: -1,
-        wuerttembergerhof: 0,
-        landsberg: 1,
-        augsburgerhof: 2,
+        casino: -3,
+        wuerttembergerhof: -3,
+        landsberg: -2,
+        cafemilani: -1,
+        augsburgerhof: 1,
         westendhall: 2,
         deutscherhof: 3,
-        donnersberg: 3,
-        cafemilani: 2
+        donnersberg: 3
     }
 },
 
 {
-    text: "Die Einzelstaaten sollen viele eigene Rechte behalten.",
-    weights: {
-        casino: 2,
-        wuerttembergerhof: 2,
-        landsberg: 1,
-        augsburgerhof: 0,
-        westendhall: -1,
-        deutscherhof: -2,
-        donnersberg: -2,
-        cafemilani: 0
-    }
-},
-
-{
-    text: "Der Staat sollte soziale Ungleichheiten aktiv bekämpfen.",
+    text: "Alle erwachsenen Männer sollen unabhängig von ihrem Vermögen wählen dürfen.",
     weights: {
         casino: -2,
         wuerttembergerhof: -1,
+        landsberg: 1,
+        cafemilani: 2,
+        augsburgerhof: 3,
+        westendhall: 3,
+        deutscherhof: 3,
+        donnersberg: 2
+    }
+},
+
+{
+    text: "Die deutschen Einzelstaaten sollen auch nach der Reichsgründung weitgehend selbstständig bleiben.",
+    weights: {
+        casino: 1,
+        wuerttembergerhof: 3,
+        landsberg: 2,
+        cafemilani: 1,
+        augsburgerhof: -1,
+        westendhall: -2,
+        deutscherhof: -2,
+        donnersberg: -2
+    }
+},
+
+{
+    text: "Deutschland braucht eine starke Zentralregierung, auch wenn die Einzelstaaten dadurch Macht verlieren.",
+    weights: {
+        casino: 2,
+        wuerttembergerhof: -2,
+        landsberg: 1,
+        cafemilani: 1,
+        augsburgerhof: 2,
+        westendhall: 2,
+        deutscherhof: 3,
+        donnersberg: 3
+    }
+},{
+    text: "Der Staat soll die Lebensbedingungen der Arbeiter aktiv verbessern, auch wenn dafür stärker in die Wirtschaft eingegriffen werden muss.",
+    weights: {
+        casino: -2,
+        wuerttembergerhof: -2,
         landsberg: 0,
+        cafemilani: 1,
+        augsburgerhof: 2,
+        westendhall: 2,
+        deutscherhof: 3,
+        donnersberg: 3
+    }
+},
+
+{
+    text: "Politische Veränderungen dürfen notfalls auch durch eine Revolution erreicht werden.",
+    weights: {
+        casino: -3,
+        wuerttembergerhof: -3,
+        landsberg: -2,
+        cafemilani: -1,
         augsburgerhof: 1,
         westendhall: 2,
         deutscherhof: 2,
-        donnersberg: 3,
-        cafemilani: 1
+        donnersberg: 3
     }
 },
 
 {
-    text: "Die Presse sollte möglichst frei berichten dürfen.",
+    text: "Die im Grundrechtskatalog der Paulskirchenverfassung festgeschriebenen Freiheitsrechte müssen dauerhaft geschützt werden.",
     weights: {
-        casino: 2,
-        wuerttembergerhof: 2,
-        landsberg: 2,
-        augsburgerhof: 2,
-        westendhall: 2,
-        deutscherhof: 2,
-        donnersberg: 2,
-        cafemilani: 2
+        casino: 3,
+        wuerttembergerhof: 3,
+        landsberg: 3,
+        cafemilani: 3,
+        augsburgerhof: 3,
+        westendhall: 3,
+        deutscherhof: 3,
+        donnersberg: 3
     }
 },
 
 {
-    text: "Der König darf politische Entscheidungen nicht allein treffen.",
+    text: "Die Regierung soll dem Parlament verantwortlich sein und von ihm kontrolliert werden.",
     weights: {
         casino: 1,
         wuerttembergerhof: 2,
-        landsberg: 2,
-        augsburgerhof: 2,
+        landsberg: 3,
+        cafemilani: 3,
+        augsburgerhof: 3,
         westendhall: 3,
         deutscherhof: 3,
-        donnersberg: 3,
-        cafemilani: 2
+        donnersberg: 2
     }
 },
 
 {
-    text: "Eine Republik ist besser als eine Monarchie.",
+    text: "Die deutsche Einheit ist wichtiger als die bisherigen Machtansprüche der Fürsten.",
     weights: {
-        casino: -3,
-        wuerttembergerhof: -2,
-        landsberg: -1,
-        augsburgerhof: 0,
-        westendhall: 2,
-        deutscherhof: 3,
-        donnersberg: 3,
-        cafemilani: 1
-    }
-},
-
-{
-    text: "Grundrechte müssen dauerhaft in einer Verfassung garantiert werden.",
-    weights: {
-        casino: 2,
+        casino: 3,
         wuerttembergerhof: 2,
-        landsberg: 2,
-        augsburgerhof: 2,
-        westendhall: 2,
-        deutscherhof: 2,
-        donnersberg: 2,
-        cafemilani: 2
-    }
-},
-
-{
-    text: "Die Bevölkerung sollte stärker an politischen Entscheidungen beteiligt werden.",
-    weights: {
-        casino: -1,
-        wuerttembergerhof: 0,
-        landsberg: 1,
-        augsburgerhof: 2,
+        landsberg: 3,
+        cafemilani: 2,
+        augsburgerhof: 3,
         westendhall: 3,
-        deutscherhof: 3,
-        donnersberg: 3,
-        cafemilani: 2
-    }
-},
-
-{
-    text: "Eine Revolution ist ein legitimes Mittel, um politische Veränderungen zu erreichen.",
-    weights: {
-        casino: -3,
-        wuerttembergerhof: -2,
-        landsberg: -1,
-        augsburgerhof: 0,
-        westendhall: 1,
         deutscherhof: 2,
-        donnersberg: 3,
-        cafemilani: 0
+        donnersberg: 1
     }
 }
 
