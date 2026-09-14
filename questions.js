8a11602a562fd38310b4253a222e2db552a33735
posch-m <posch-m@web.de>
2026-09-14 18:46:45 +0200

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,149 +1,142 @@
-/*
-========================================================
-FraktionsFinder 1848
-questions.js
-Teil 1 von 2
-========================================================
-*/
-
 const questions = [
-
-{
-    text: "Deutschland soll eine konstitutionelle Monarchie mit einem erblichen Kaiser als Staatsoberhaupt erhalten.",
-    weights: {
-        casino: 3,
-        wuerttembergerhof: 3,
-        landsberg: 2,
-        cafemilani: 1,
-        augsburgerhof: -1,
-        westendhall: -2,
-        deutscherhof: -3,
-        donnersberg: -3
+    {
+        id: "p4rex4",
+        text: "Soll Deutschland eine konstitutionelle Monarchie mit einem erblichen Kaiser an der Spitze werden?",
+        weights: {
+            casino: 3,
+            wuerttembergerhof: 2,
+            landsberg: 3,
+            cafemilani: 3,
+            augsburgerhof: 2,
+            westendhall: -2,
+            deutscherhof: -3,
+            donnersberg: -3
+        }
+    },
+    {
+        id: "rayqj3",
+        text: "Soll Deutschland eine Republik werden, in der die politische Macht vom Volk ausgeht?",
+        weights: {
+            casino: -3,
+            wuerttembergerhof: -3,
+            landsberg: -3,
+            cafemilani: -3,
+            augsburgerhof: -2,
+            westendhall: 2,
+            deutscherhof: 3,
+            donnersberg: 3
+        }
+    },
+    {
+        id: "6jh0r7",
+        text: "Soll das allgemeine und gleiche Wahlrecht für Männer eingeführt werden?",
+        weights: {
+            casino: -2,
+            wuerttembergerhof: 0,
+            landsberg: -1,
+            cafemilani: -3,
+            augsburgerhof: 1,
+            westendhall: 3,
+            deutscherhof: 3,
+            donnersberg: 3
+        }
+    },
+    {
+        id: "0miasa",
+        text: "Sollten die einzelnen deutschen Staaten ihre Eigenständigkeit und ihre bisherigen Rechte weitgehend behalten?",
+        weights: {
+            casino: 1,
+            wuerttembergerhof: 2,
+            landsberg: 1,
+            cafemilani: 3,
+            augsburgerhof: 1,
+            westendhall: -1,
+            deutscherhof: -2,
+            donnersberg: -2
+        }
+    },
+    {
+        id: "88tuj8",
+        text: "Braucht ein geeintes Deutschland eine starke zentrale Regierung?",
+        weights: {
+            casino: 3,
+            wuerttembergerhof: 1,
+            landsberg: 2,
+            cafemilani: -2,
+            augsburgerhof: 2,
+            westendhall: 1,
+            deutscherhof: 2,
+            donnersberg: 2
+        }
+    },
+    {
+        id: "a15asz",
+        text: "Soll der Staat stärker in soziale und wirtschaftliche Fragen eingreifen, um soziale Not zu bekämpfen?",
+        weights: {
+            casino: -2,
+            wuerttembergerhof: -1,
+            landsberg: -1,
+            cafemilani: -2,
+            augsburgerhof: 1,
+            westendhall: 2,
+            deutscherhof: 3,
+            donnersberg: 3
+        }
+    },
+    {
+        id: "lkwxoe",
+        text: "Soll die Revolution von 1848 konsequent weitergeführt werden, auch wenn dadurch bestehende Herrschaftsverhältnisse gefährdet werden?",
+        weights: {
+            casino: -3,
+            wuerttembergerhof: -3,
+            landsberg: -3,
+            cafemilani: -3,
+            augsburgerhof: -1,
+            westendhall: 1,
+            deutscherhof: 2,
+            donnersberg: 3
+        }
+    },
+    {
+        id: "ou9tsn",
+        text: "Soll der neue deutsche Staat umfassende Grund- und Freiheitsrechte garantieren?",
+        weights: {
+            casino: 2,
+            wuerttembergerhof: 2,
+            landsberg: 2,
+            cafemilani: 1,
+            augsburgerhof: 3,
+            westendhall: 3,
+            deutscherhof: 3,
+            donnersberg: 3
+        }
+    },
+    {
+        id: "osxdtc",
+        text: "Soll die Regierung vom Vertrauen des Parlaments abhängig sein und von ihm kontrolliert werden?",
+        weights: {
+            casino: -1,
+            wuerttembergerhof: 3,
+            landsberg: 2,
+            cafemilani: -3,
+            augsburgerhof: 3,
+            westendhall: 3,
+            deutscherhof: 3,
+            donnersberg: 3
+        }
+    },
+    {
+        id: "ubxkd6",
+        text: "Soll die nationale Einheit wichtiger sein als die Interessen der einzelnen Fürsten und Staaten?",
+        weights: {
+            casino: 3,
+            wuerttembergerhof: 1,
+            landsberg: 2,
+            cafemilani: -2,
+            augsburgerhof: 2,
+            westendhall: 3,
+            deutscherhof: 3,
+            donnersberg: 2
+        }
     }
-},
-
-{
-    text: "Deutschland sollte statt einer Monarchie eine Republik werden.",
-    weights: {
-        casino: -3,
-        wuerttembergerhof: -3,
-        landsberg: -2,
-        cafemilani: -1,
-        augsburgerhof: 1,
-        westendhall: 2,
-        deutscherhof: 3,
-        donnersberg: 3
-    }
-},
-
-{
-    text: "Alle erwachsenen Männer sollen unabhängig von ihrem Vermögen wählen dürfen.",
-    weights: {
-        casino: -2,
-        wuerttembergerhof: -1,
-        landsberg: 1,
-        cafemilani: 2,
-        augsburgerhof: 3,
-        westendhall: 3,
-        deutscherhof: 3,
-        donnersberg: 2
-    }
-},
-
-{
-    text: "Die deutschen Einzelstaaten sollen auch nach der Reichsgründung weitgehend selbstständig bleiben.",
-    weights: {
-        casino: 1,
-        wuerttembergerhof: 3,
-        landsberg: 2,
-        cafemilani: 1,
-        augsburgerhof: -1,
-        westendhall: -2,
-        deutscherhof: -2,
-        donnersberg: -2
-    }
-},
-
-{
-    text: "Deutschland braucht eine starke Zentralregierung, auch wenn die Einzelstaaten dadurch Macht verlieren.",
-    weights: {
-        casino: 2,
-        wuerttembergerhof: -2,
-        landsberg: 1,
-        cafemilani: 1,
-        augsburgerhof: 2,
-        westendhall: 2,
-        deutscherhof: 3,
-        donnersberg: 3
-    }
-},{
-    text: "Der Staat soll die Lebensbedingungen der Arbeiter aktiv verbessern, auch wenn dafür stärker in die Wirtschaft eingegriffen werden muss.",
-    weights: {
-        casino: -2,
-        wuerttembergerhof: -2,
-        landsberg: 0,
-        cafemilani: 1,
-        augsburgerhof: 2,
-        westendhall: 2,
-        deutscherhof: 3,
-        donnersberg: 3
-    }
-},
-
-{
-    text: "Politische Veränderungen dürfen notfalls auch durch eine Revolution erreicht werden.",
-    weights: {
-        casino: -3,
-        wuerttembergerhof: -3,
-        landsberg: -2,
-        cafemilani: -1,
-        augsburgerhof: 1,
-        westendhall: 2,
-        deutscherhof: 2,
-        donnersberg: 3
-    }
-},
-
-{
-    text: "Die im Grundrechtskatalog der Paulskirchenverfassung festgeschriebenen Freiheitsrechte müssen dauerhaft geschützt werden.",
-    weights: {
-        casino: 3,
-        wuerttembergerhof: 3,
-        landsberg: 3,
-        cafemilani: 3,
-        augsburgerhof: 3,
-        westendhall: 3,
-        deutscherhof: 3,
-        donnersberg: 3
-    }
-},
-
-{
-    text: "Die Regierung soll dem Parlament verantwortlich sein und von ihm kontrolliert werden.",
-    weights: {
-        casino: 1,
-        wuerttembergerhof: 2,
-        landsberg: 3,
-        cafemilani: 3,
-        augsburgerhof: 3,
-        westendhall: 3,
-        deutscherhof: 3,
-        donnersberg: 2
-    }
-},
-
-{
-    text: "Die deutsche Einheit ist wichtiger als die bisherigen Machtansprüche der Fürsten.",
-    weights: {
-        casino: 3,
-        wuerttembergerhof: 2,
-        landsberg: 3,
-        cafemilani: 2,
-        augsburgerhof: 3,
-        westendhall: 3,
-        deutscherhof: 2,
-        donnersberg: 1
-    }
-}
-
 ];
